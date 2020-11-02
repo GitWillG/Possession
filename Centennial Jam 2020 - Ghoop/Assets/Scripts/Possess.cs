98e998b56a5412ebb961632746ab9ddf4eda9f1a
GitWillG <wgrant14@hastings.edu>
2020-11-02 17:52:34 -0500

    UnGhostify works
    Press E to unghostify

--- Centennial Jam 2020 - Ghoop/Assets/Scripts/Possess.cs
+++ Centennial Jam 2020 - Ghoop/Assets/Scripts/Possess.cs
@@ -8,28 +8,49 @@ public class Possess : MonoBehaviour
     public GameObject Corpse;
     // Start is called before the first frame update
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && Corpse != null)
+        {
+            Debug.Log("E");
+            possessCorpse();
+
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Corpse")
         {
-            Debug.Log("collision");
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                Debug.Log("E");
-                possessCorpse();
+            Corpse = collision.gameObject;
 
-            }
+            Debug.Log("collision");
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Corpse")
+        {
+            Corpse = null;
+
         }
     }
 
 
+
     public void possessCorpse()
     {
-        this.GetComponent<Collider2D>().isTrigger = true;
-        this.GetComponent<playerController>().enabled = false;
-        this.gameObject.tag = "Corpse";
-        GameObject ghostOb = Instantiate(ghost, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
-        ghostOb.transform.parent = null;
-        Camera.main.GetComponent<CameraFollower>().target = ghostOb.transform;
+        Debug.Log("test");
+        Corpse.GetComponent<Collider2D>().isTrigger = false;
+        Corpse.GetComponent<playerController>().enabled = true;
+        Camera.main.GetComponent<CameraFollower>().target = Corpse.transform;
+        Destroy(this.gameObject);
+        //this.GetComponent<Collider2D>().isTrigger = true;
+        //this.GetComponent<playerController>().enabled = false;
+        //this.gameObject.tag = "Corpse";
+        //GameObject ghostOb = Instantiate(ghost, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+        //ghostOb.transform.parent = null;
+        //Camera.main.GetComponent<CameraFollower>().target = ghostOb.transform;
     }
 }
