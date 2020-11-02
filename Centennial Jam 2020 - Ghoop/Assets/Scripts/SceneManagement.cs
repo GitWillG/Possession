1be055479d55170ef4ffe8a00fbb48961c475463
QA3R <46014750+QA3R@users.noreply.github.com>
2020-11-02 14:08:06 -0500

    Added folders and Setup SceneManager
    - Reorganized folders for project
    - Added Level 01 Scene, Main Menu Scene and Player Scene
    - Added SceneManagement Script (supports single and additive loading)
    - Added TextMeshProUGUI

---
+++ Centennial Jam 2020 - Ghoop/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    private static SceneManagement _instance;
+    public static SceneManagement instance { get { return _instance; } }
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            //Debug.Log("Instance was found");
+        }
+        else
+        {
+            _instance = this;
+            //Debug.Log("Instance was not found");
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Loads the scene using LoadSceneMode.Additive
+    public void LoadSceneAdditive(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Additive);
+        //Debug.Log(sceneIndex + " was loaded");
+        Time.timeScale = 1;
+    }
+
+    // Loads the scene using LoadSceneMode.Single
+    public void LoadSceneSingle(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+        //Debug.Log(sceneIndex + " was loaded");
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        //Debug.Log("Game Successfully Closed");
+    }
+}
