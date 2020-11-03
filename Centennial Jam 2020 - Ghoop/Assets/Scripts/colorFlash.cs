61dbe369b977749afbb092ba7385f8fb114c9777
ArdentDuck <73841343+ArdentDuck@users.noreply.github.com>
2020-11-03 13:52:45 -0500

    Main Menu Screen Update
    Scenes > Nic Scene

---
+++ Centennial Jam 2020 - Ghoop/Assets/Scripts/colorFlash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class colorFlash : MonoBehaviour
+{
+    Color lightRed = new Color(0.99f, 0.49f, 0.49f, 1f);
+    Color darkGrey = new Color(0.09f, 0.09f, 0.09f, 1f);
+    Color myVar; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        myVar = GetComponent<RawImage>().color;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       myVar = Color.Lerp(darkGrey, lightRed, Mathf.PingPong(Time.time, 1));
+       this.gameObject.GetComponent<RawImage>().color = myVar;
+    }
+}
--- Centennial Jam 2020 - Ghoop/Assets/Scripts/lightSpinner.cs
+++ Centennial Jam 2020 - Ghoop/Assets/Scripts/lightSpinner.cs
@@ -13,6 +13,6 @@ public class lightSpinner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.5f);
+        transform.Rotate(0, 0, 0.35f);
     }
 }
