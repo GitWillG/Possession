c8959ce5acdcab9b28aac9a75849d0a4229934be
GitWillG <wgrant14@hastings.edu>
2020-11-05 16:59:48 -0500

    auto assign animator
    titlt

--- Centennial Jam 2020 - Ghoop/Assets/Scripts/Controllables/Animators/PlayerAnimator.cs
+++ Centennial Jam 2020 - Ghoop/Assets/Scripts/Controllables/Animators/PlayerAnimator.cs
@@ -8,7 +8,7 @@ public class PlayerAnimator : EntityAnimator
     // Start is called before the first frame update
     void Start()
     {
-
+        this.Anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
