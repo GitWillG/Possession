fa3443baad7e1048f02bd698c8aec672d0d6a6e2
QA3R <46014750+QA3R@users.noreply.github.com>
2020-11-05 23:42:23 -0500

    Added Post-Processing

---
+++ Centennial Jam 2020 - Ghoop/Assets/SingletonImplementation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonImplementation : MonoBehaviour
+{
+    private static SingletonImplementation _instance;
+    public static SingletonImplementation Instance { get { return _instance; } }
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            //Debug.Log("Instance was found");
+        }
+        else
+        {
+            _instance = this;
+            //Debug.Log("Instance was not found");
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
